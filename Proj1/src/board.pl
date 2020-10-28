f670e02ce975d00abeed3528ecfe5cdcda1f3805
João Pinto <joaocastropintoz@gmail.com>
2020-10-28 09:19:56 +0000

    added Skull and changeSkull

--- Proj1/src/board.pl
+++ Proj1/src/board.pl
@@ -1,3 +1,5 @@
+:- dynamic(skull/1).
+
 initial([         	               [empty],
 							    [empty, empty],
 						    [green, empty, green],
@@ -10,6 +12,24 @@ initial([         	               [empty],
 [purple, purple, purple, purple, empty, empty, white, white, white, white]]
 ).
 
+skull(purple).
+
+changeSkull(New) :-
+	skull(Current),
+	Current == purple,
+	New = green,
+	retract(skull(purple)),
+	assert(skull(green))
+.
+
+changeSkull(New) :-
+	skull(Current),
+	Current == green,
+	New = purple,
+	retract(skull(green)),
+	assert(skull(purple))
+.
+
 translate(empty, '.').
 translate(purple,'P').
 translate(green, 'G').
@@ -17,7 +37,10 @@ translate(white, 'W').
 
 display_game(GameState,_Player) :- 
 	initial(GameState),
-	printBoard(GameState, 10) %10 lines board
+	printBoard(GameState, 10), 	% 10 lines board
+	nl,
+	skull(HasSkull),
+	printSkull(HasSkull)
 .
 
 printBoard([H|T], N) :-
@@ -31,8 +54,13 @@ printBoard([],0).
 
 printLine([H|T], N) :-
 	translate(H, Transl),
-	format('~*c| ~w ', [N*2, 0' , Transl]),
+	format('~*c| ~w ', [N*2, 0 , Transl]),
 	printLine(T, 0)
 .
 printLine([], 0).
 
+printSkull(HasSkull) :-
+	write(HasSkull),
+	write(' has the Skull')
+.
+	
